2d0f0f013fff6afc6d721792bae2caddc363a50a
Roman Minaev <romario123321@yandex.ru>
2024-08-19 22:58:52 +0300

    Reg Admin models

--- product/users/admin.py
+++ product/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import CustomUser, Balance, Subscription, Course
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_author')
+    list_filter = ('is_author',)
+    search_fields = ('email', 'username')
+
+
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('amount',)
+    list_filter = ('amount',)
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course')
+    list_filter = ('user', 'course')
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    list_filter = ('author',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Balance, BalanceAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(Course, CourseAdmin)
