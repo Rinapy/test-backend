54e17c2192761fd0a45a9e1031065c4c6678e6df
Roman Minaev <romario123321@yandex.ru>
2024-08-19 22:59:58 +0300

    Modify models

--- product/users/models.py
+++ product/users/models.py
@@ -1,14 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import UniqueConstraint, CheckConstraint
+from django.db.models import F
+
+from courses.models import Group
+
+from core.enums import Limits
+
 
 class CustomUser(AbstractUser):
     """Кастомная модель пользователя - студента."""
 
     email = models.EmailField(
         verbose_name='Адрес электронной почты',
-        max_length=250,
-        unique=True
+        unique=True,
+        max_length=250
     )
+
+    balance = models.ForeignKey(
+        'Balance',
+        on_delete=models.CASCADE,
+        verbose_name='Баланс пользователя',
+        related_name='balance'
+    )
+
+    is_author = models.BooleanField(
+        verbose_name='Пользователь являеться преподователем.'
+    )
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = (
         'username',
@@ -29,7 +49,16 @@ class CustomUser(AbstractUser):
 class Balance(models.Model):
     """Модель баланса пользователя."""
 
-    # TODO
+    amount = models.PositiveIntegerField(
+        verbose_name='Баланс',
+        default=1000,
+        validators=[
+            MinValueValidator(
+                Limits.MIN_BALANCE_AMOUNT,
+                message='Минимальный баланс пользователя 0!'
+            ),
+        ]
+    )
 
     class Meta:
         verbose_name = 'Баланс'
@@ -40,10 +69,34 @@ class Balance(models.Model):
 class Subscription(models.Model):
     """Модель подписки пользователя на курс."""
 
-    # TODO
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='subscriber',
+        verbose_name="Подписчик",
+        on_delete=models.CASCADE,
+    )
+    # course = models.ForeignKey(
+    #     Course,
+    #     related_name='subscribing',
+    #     verbose_name="Курс",
+    #     on_delete=models.CASCADE,
+    # )
+    group = models.ManyToManyField(
+        Group,
+        related_name='subscribers',
+        on_delete=models.CASCADE,
+        verbose_name='Группа',
+    )
 
     class Meta:
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
         ordering = ('-id',)
-
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'group__course'],
+                name='unique_subscription'),
+            CheckConstraint(
+                check=~(models.Q(user=F('group__course__author'))),
+                name='author_cannot_subscribe_to_own_course')
+        ]
